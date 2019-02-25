73023b33f9476526fa989a83ab4d043ef9623f16
priyanka <priyanka.pandit@cuelogic.com>
2019-02-25 17:42:18 +0530

    commiting clock application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      current: new Date()
+    }
+  }
+
+  componentDidMount(){
+    this.timerID = setInterval( () => this.tick(),1000 );
+  }
+  tick() {
+    this.setState({
+      current: new Date()
+    });
+  }
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <h2>Clock Application</h2>
+          <h2>{this.state.current.toLocaleTimeString()}</h2>
       </div>
     );
   }
